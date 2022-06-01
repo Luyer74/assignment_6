3be5b134e52fde675f6b9fe939b75a1596d97e51
Luis Yerik Arambula <A00825080@itesm.mx>
2022-06-01 00:03:33 -0500

    add test cases and make products slaves

--- ass6.erl
+++ ass6.erl
@@ -5,15 +5,13 @@
 -export([
     open_store/0,
     store/1,
-    partner/1,
+    product/2,
     subscribe_partner/1,
     list_partners/0,
     delete_partner/1,
     register_product/2,
     remove_product/1,
     modify_stock/2,
-    stock_list/0,
-    stock_list/1,
     close_store/0,
     test/0
 ]).
@@ -55,73 +53,67 @@ remove_product(Product) ->
 modify_stock(Product, Quantity) ->
     {getAlias(), getNode(getAlias())} ! {modify_stock, Product, Quantity}.
 
-% stock list for store
-stock_list() ->
-    {getAlias(), getNode(getAlias())} ! {stock_list}.
-
-% stock list for partner
-stock_list(Partner) ->
-    {getAlias(), getNode(getAlias())} ! {stock_list, Partner}.
-
 store(Alias) -> store(Alias, [], []).
 store(Alias, Partners, Products) ->
     receive
         {subscribe_partner, Partner} ->
-            io:format("Subscribing..."),
-            Alive = net_adm:ping(getNode(Partner)),
+            AddedPreviously = lists:any(fun(X) -> X == Partner end, Partners),
             if
-                Alive == pang ->
-                    io:format("PartnerNode ~p is down~n", [getNode(Partner)]),
+                % already exists partner
+                AddedPreviously ->
+                    io:format("Partner ~p already exist~n", [Partner]),
                     store(Alias, Partners, Products);
                 true ->
-                    AddedPreviously = lists:any(fun({X, _}) -> X == Partner end, Partners),
-                    if
-                        % already exists partner
-                        AddedPreviously ->
-                            io:format("Partner ~p already exist~n", [Partner]),
-                            store(Alias, Partners, Products);
-                        true ->
-                            Pid = spawn(getNode(Partner), ?MODULE, partner, [Partner]),
-                            case rpc:call(getNode(Partner), erlang, is_process_alive, [Pid]) of
-                                true ->
-                                    io:format("~p partner created in node ~p~n", [
-                                        Partner, getNode(Partner)
-                                    ]),
-                                    store(Alias, Partners ++ [{Partner, Pid}], Products);
-                                false ->
-                                    io:format("node ~p does not exist~n", [getNode(Partner)]),
-                                    store(Alias, Partners, Products)
-                            end
-                    end
+                    io:format("~p partner created ~n", [
+                        Partner
+                    ]),
+                    store(Alias, Partners ++ [Partner], Products)
             end;
         {delete_partner, Partner} ->
-            AddedPreviously = lists:any(fun({X, _}) -> X == Partner end, Partners),
+            AddedPreviously = lists:any(fun(X) -> X == Partner end, Partners),
             if
                 AddedPreviously ->
                     io:format("Deleted partner ~p ~n", [Partner]),
-                    Pid = get_person_pid(Partner, Partners),
-                    Pid ! {delete},
-                    NewList = lists:filter(fun({Name, _}) -> Name /= Partner end, Partners),
-                    store(Alias, NewList, Products);
+                    store(Alias, lists:delete(Partner, Partners), Products);
                 true ->
                     io:format("Partner ~p does not exist~n", [Partner]),
                     store(Alias, Partners, Products)
             end;
         {register_product, Product, Quantity} ->
             AddedPreviously = lists:any(fun({P, _}) -> P == Product end, Products),
+            Alive = net_adm:ping(getNode(Product)),
             if
-                AddedPreviously ->
-                    io:format("Product ~p already exists ~n", [Product]),
+                Alive == pang ->
+                    io:format("node ~p is down~n", [getNode(Product)]),
                     store(Alias, Partners, Products);
                 true ->
-                    io:format("Added product  ~p with quantity ~p ~n", [Product, Quantity]),
-                    store(Alias, Partners, Products ++ [{Product, Quantity}])
+                    if
+                        AddedPreviously ->
+                            io:format("Product ~p already exists ~n", [Product]),
+                            store(Alias, Partners, Products);
+                        true ->
+                            Pid = spawn(getNode(Product), ?MODULE, product, [Product, Quantity]),
+                            case rpc:call(getNode(Product), erlang, is_process_alive, [Pid]) of
+                                true ->
+                                    io:format(
+                                        "~p product slave with quantity ~p created in node ~p~n", [
+                                            Product, Quantity, getNode(Product)
+                                        ]
+                                    ),
+                                    store(Alias, Partners, Products ++ [{Product, Pid}]);
+                                false ->
+                                    io:format("node ~p does not exist~n", [getNode(Product)]),
+                                    store(Alias, Partners, Products)
+                            end
+                    end
             end;
         {remove_product, Product} ->
             AddedPreviously = lists:any(fun({P, _}) -> P == Product end, Products),
             if
                 AddedPreviously ->
                     io:format("Removed product ~p ~n", [Product]),
+                    Pid = get_pid(Product, Products),
+                    Pid ! {delete},
                     store(Alias, Partners, delete_product_list(Product, Products));
                 true ->
                     io:format("Product ~p does not exist~n", [Product]),
@@ -132,42 +124,33 @@ store(Alias, Partners, Products) ->
             if
                 AddedPreviously ->
                     io:format("Modified stock of ~p ~n", [Product]),
-                    store(Alias, Partners, modify_stock_list({Product, Quantity}, Products));
+                    Pid = get_pid(Product, Products),
+                    Pid ! {modify, Quantity},
+                    store(Alias, Partners, Products);
                 true ->
                     io:format("Product ~p does not exist~n", [Product]),
                     store(Alias, Partners, Products)
             end;
-        {stock_list, Partner} ->
-            AddedPreviously = lists:any(fun({X, _}) -> X == Partner end, Partners),
-            if
-                AddedPreviously ->
-                    Pid = get_person_pid(Partner, Partners),
-                    Pid ! {stock_list, Products},
-                    store(Alias, Partners, Products);
-                true ->
-                    io:format("Partner ~p does not exist~n", [Partner]),
-                    store(Alias, Partners, Products)
-            end;
-        {stock_list} ->
-            io:fwrite(user, "~w~n", [Products]),
-            store(Alias, Partners, Products);
         {list_partners} ->
             io:fwrite("~w~n", [Partners]),
             store(Alias, Partners, Products);
         {close} ->
+            RemoveProduct = fun({_, Pid}) -> Pid ! {remove} end,
+            lists:foreach(RemoveProduct, Products),
             io:format("Closing store ~n")
     end.
 
-partner(Alias) ->
+product(Product, Quantity) ->
     receive
-        {msg, Msg} ->
-            io:format(user, "Partner ~p recieved ~p ~n", [Alias, Msg]),
-            partner(Alias);
-        {delete} ->
-            io:format(user, "Partner ~p was deleted  ~n", [Alias]);
-        {stock_list, Products} ->
-            io:fwrite(user, "~w~n", [Products]),
-            partner(Alias)
+        {remove} ->
+            io:format(user, "Product ~p was removed  ~n", [Product]);
+        {modify, Q} ->
+            if
+                Q + Quantity >= 0 ->
+                    product(Product, Q + Quantity);
+                true ->
+                    product(Product, Quantity)
+            end
     end.
 
 delete_product_list(_, []) ->
@@ -180,24 +163,14 @@ delete_product_list(P, [{PD, QD} | R]) ->
             [{PD, QD}] ++ delete_product_list(P, R)
     end.
 
-modify_stock_list(_, []) ->
-    [];
-modify_stock_list({P, Q}, [{PD, QD} | R]) ->
-    if
-        (P == PD) and (Q + QD >= 0) ->
-            [{PD, QD + Q}] ++ modify_stock_list({P, Q}, R);
-        true ->
-            [{PD, QD}] ++ modify_stock_list({P, Q}, R)
-    end.
-
-get_person_pid(_, []) ->
+get_pid(_, []) ->
     -1;
-get_person_pid(P, [{PD, PID} | R]) ->
+get_pid(P, [{PD, PID} | R]) ->
     if
         P == PD ->
             PID;
         true ->
-            get_person_pid(P, R)
+            get_pid(P, R)
     end.
 
 test()->
